72cddf22700f73077f2c4b414112d4c5f70c6755
Bartel, Matt <mattbartel3@gmail.com>
2021-04-04 18:39:07 -0500

    use status led, use average of startup values to set threshold, require multiple active values before becoming active

--- VibrationSensor.h
+++ VibrationSensor.h
@@ -5,30 +5,70 @@
 #define TIME_WINDOW 3000 // 3 secs
 #define TIME_UNTIL_ON 30000 // 30 secs
 #define TIME_UNTIL_DONE 300000 // 5 mins
+#define STARTUP_SAMPLES 30 // number of readings to average on startup to determine a baseline
+#define ACTIVE_READINGS 2 // number of active readings to require within TIME_WINDOW before becoming active
 
 enum ApplianceState {
   IDLE,
   MAYBE_ON,
   ON,
   MAYBE_DONE,
-  DONE
+  DONE,
+  STARTUP
 };
 
 class VibrationSensor : public PollingComponent, public Sensor {
   private:
+    uint8_t startupIndex = 0;
+    sensors_vec_t *startupValues;
+    const char *name;
     uint8_t address;
     bool error;
     float initialX, initialY, initialZ, threshold;
-    long lastActive, lastIdle;
+    uint8_t activeReadings;
+    long lastMaybeActive, lastActive, lastIdle;
     ApplianceState state;
+
+    // Average the first STARTUP_SAMPLES readings from the sensor to determine the baseline
+    bool buildStartSamples(sensors_event_t *sEvent) {
+      startupValues[startupIndex] = sEvent->acceleration;
+      startupIndex++;
+
+      bool startupComplete = false;
+      if (startupIndex >= STARTUP_SAMPLES) {
+        state = IDLE;
+
+        for (uint8_t k = 0; k < STARTUP_SAMPLES; k++) {
+          initialX += startupValues[k].x;
+          initialY += startupValues[k].y;
+          initialZ += startupValues[k].z;
+        }
+
+        delete[] startupValues;
+
+        initialX /= STARTUP_SAMPLES;
+        initialY /= STARTUP_SAMPLES;
+        initialZ /= STARTUP_SAMPLES;
+
+        // make sure we start IDLE
+        lastActive = millis() - (TIME_WINDOW + (TIME_WINDOW/2));
+
+        startupComplete = true;
+      }
+
+      publish_state(state);
+      return startupComplete;
+    }
 
   public:
     Adafruit_LIS3DH lis;
 
-    VibrationSensor(uint8_t addr, float thresh) : PollingComponent(200) {
+    VibrationSensor(const char *n, uint8_t addr, float thresh) : PollingComponent(200) {
+      name = n;
       address = addr;
       threshold = thresh;
-      state = IDLE;
+      state = STARTUP;
+      startupValues = new sensors_vec_t[STARTUP_SAMPLES];
     }
 
     float get_setup_priority() const override { return esphome::setup_priority::BUS; }
@@ -36,7 +76,7 @@ class VibrationSensor : public PollingComponent, public Sensor {
     void setup() override {
       if (!lis.begin(address)) {
         error = true;
-        ESP_LOGE("custom", "failed starting LIS3DH %i", address);
+        ESP_LOGE(name, "failed starting LIS3DH 0x%x", address);
         return;
       }
 
@@ -45,15 +85,10 @@ class VibrationSensor : public PollingComponent, public Sensor {
       sensors_event_t sEvent;
       if (!lis.getEvent(&sEvent)) {
         error = true;
-        ESP_LOGE("custom", "failed reading initial state of %i", address);
+        ESP_LOGE(name, "failed reading initial state of 0x%x", address);
         return;
       }
-      initialX = sEvent.acceleration.x;
-      initialY = sEvent.acceleration.y;
-      initialZ = sEvent.acceleration.z;
-
-      // make sure we start IDLE
-      lastActive = millis() - (TIME_WINDOW + (TIME_WINDOW/2));
+      buildStartSamples(&sEvent);
     }
 
     void update() override {
@@ -63,18 +98,31 @@ class VibrationSensor : public PollingComponent, public Sensor {
 
       sensors_event_t sEvent;
       if (!lis.getEvent(&sEvent)) {
-        ESP_LOGE("custom", "failed reading state of %i", address);
+        ESP_LOGE(name, "failed reading state of 0x%x", address);
+        return;
+      }
+
+      if (state == STARTUP && !buildStartSamples(&sEvent)) {
         return;
       }
 
       float force = fabs(sEvent.acceleration.x - initialX) +
         fabs(sEvent.acceleration.y - initialY) +
         fabs(sEvent.acceleration.z - initialZ);
-      ESP_LOGD("custom", "force: %f", force);
+      ESP_LOGD(name, "0x%x force: %f", address, force);
 
       long now = millis();
       if (force > threshold) {
-        lastActive = now;
+        lastMaybeActive = now;
+        if (activeReadings >= ACTIVE_READINGS) {
+          lastActive = now;
+        }
+        else {
+          activeReadings++;
+        }
+      }
+      else if (now - lastMaybeActive >= TIME_WINDOW) {
+        activeReadings = 0;
       }
       bool recentlyActive = (now - lastActive) < TIME_WINDOW;
 
